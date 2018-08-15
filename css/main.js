1a1d398e207d9f6fc1d2a2218b9a20bd916f95d2
Erik Hoy <erikshoy@gmail.com>
2018-08-15 10:28:14 -0600

    JS Lesson 6 - 5/9

--- css/main.js
+++ css/main.js
@@ -1,7 +1,9 @@
 let newYearsResolutions = ['Get a great job', 'Paint the house', 'Travel more'];
-newYearsResolutions[1] = 'Learn a new language';
-
 let listItem = newYearsResolutions[0];
 console.log(listItem);
 console.log(newYearsResolutions[2]);
 console.log(newYearsResolutions[3]);
+
+newYearsResolutions[1] = 'Learn a new language';
+
+console.log(newYearsResolutions.length);
