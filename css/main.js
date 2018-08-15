3f747d5054a3a8645b488c2bb7338d8e64ecd7e4
Erik Hoy <erikshoy@gmail.com>
2018-08-15 12:22:40 -0600

    JS Lesson 9 - 5/15

--- css/main.js
+++ css/main.js
@@ -1,7 +1,19 @@
 let person = {
     name: 'Erik',
-    age: 44
+    age: 44,
+    weekendAlarm: 'No alarms needed',
+    weekAlarm: 'Alarm set to 7am'
   };
+  
+  let day = 'Wednesday';
+  let alarm;
+  
+  if(day === 'Saturday' || day === 'Sunday') {
+    alarm = 'weekendAlarm';
+  } else {
+    alarm = 'weekAlarm';
+  }
   
   console.log(person['name']);
   console.log(person['age']);
+  console.log(person[alarm]);
