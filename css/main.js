1970a277f5d79969d3d2c367feee54e05fc5b18e
Erik Hoy <erikshoy@gmail.com>
2018-08-15 11:17:37 -0600

    JS Lesson 7 - 3/8

--- css/main.js
+++ css/main.js
@@ -1,4 +1,5 @@
 let vacationSpots = ['Philadelphia', 'New York City', 'Los Angeles'];
-console.log(vacationSpots[0]);
-console.log(vacationSpots[1]);
-console.log(vacationSpots[2]);
+
+for (let vacationSpotIndex=0; vacationSpotIndex<vacationSpots.length; vacationSpotIndex++) {
+  console.log(`I would love to visit ${vacationSpots[vacationSpotIndex]}`);
+}
