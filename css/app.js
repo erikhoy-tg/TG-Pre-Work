9c61e21ba6acb3a209a4ede398fe26066b76b4c5
Erik Hoy <erikshoy@gmail.com>
2018-08-15 09:33:23 -0600

    JS Lesson 4 - 4/10

--- css/app.js
+++ css/app.js
@@ -1,5 +1,7 @@
-const takeOrder = (topping) => {
-    console.log(`Order: pizza topped with ${topping}`);
+const takeOrder = (topping, crustType) => {
+    console.log(`Order: ${crustType} pizza topped with + ${topping}`);
   };
   
-  takeOrder('pineapples');
+  takeOrder('mushrooms', 'thin');
+  takeOrder('pineapples', 'hand tossed');
+  takeOrder('black olives', 'deep dish');
