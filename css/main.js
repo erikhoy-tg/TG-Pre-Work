7731467d5e14634d6a1897772b42cc2f05dcfd59
Erik Hoy <erikshoy@gmail.com>
2018-08-15 11:44:53 -0600

    JS Lesson 8 - 3/7

--- css/main.js
+++ css/main.js
@@ -1,4 +1,13 @@
-let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+let animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
 
-// Iterate over fruits below
-fruits.forEach(fruit => console.log(fruit));
+// Create the secretMessage array below
+let secretMessage = animals.map(animalChar => animalChar[0]);
+
+console.log(secretMessage.join(''));
+
+let bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+let smallNumbers = bigNumbers.map(function(n) {
+  return n/100;
+});
