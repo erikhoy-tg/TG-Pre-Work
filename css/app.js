a67023f69c1e6062be4437803a8865dcebcab8a6
Erik Hoy <erikshoy@gmail.com>
2018-08-15 10:01:31 -0600

    JS Lesson 4 - 9/10

--- css/app.js
+++ css/app.js
@@ -1,9 +1,4 @@
-const isGreaterThan = (numberOne, numberTwo) => {
-    if(numberOne > numberTwo){
-      return true;
-    } else {
-      return false;
-    }
-  }
-  
-  isGreaterThan(4, 8)
+const volumeOfSphere = diameter => 
+  (1/6) * Math.PI * diameter * diameter * diameter;
+
+console.log('The volume of a sphere is ' + volumeOfSphere(10) + ' cubic centimeters');
