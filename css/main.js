df8ed8fa7b03d02556a133d5e68deeea78b1338a
Erik Hoy <erikshoy@gmail.com>
2018-08-15 17:46:07 -0600

    JS Lesson 11 - 10/11

--- css/main.js
+++ css/main.js
@@ -1,9 +1,10 @@
+"use strict";
+
 var pasta = "Spaghetti"; // ES5 syntax
 
-const meat = "Pancetta"; // ES6 syntax
+var meat = "Pancetta"; // ES6 syntax
 
-let sauce = "Eggs and cheese"; // ES6 syntax
+var sauce = "Eggs and cheese"; // ES6 syntax
 
 // Template literals, like the one below, were introduced in ES6
-const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
-
+var carbonara = "You can make carbonara with " + pasta + ", " + meat + ", and a sauce made with " + sauce + ".";
