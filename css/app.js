34ce52f05a09225c72120238070fc88bca5093d7
Erik Hoy <erikshoy@gmail.com>
2018-08-15 09:48:13 -0600

    JS Lesson 4 - 5/10

--- css/app.js
+++ css/app.js
@@ -1,7 +1,15 @@
+let orderCount = 0;
 const takeOrder = (topping, crustType) => {
-    console.log(`Order: ${crustType} pizza topped with + ${topping}`);
-  };
-  
-  takeOrder('mushrooms', 'thin');
-  takeOrder('pineapples', 'hand tossed');
-  takeOrder('black olives', 'deep dish');
+  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
+  orderCount += 1;
+};
+
+const getSubTotal = (itemCount) => {
+  return (itemCount * 7.50);
+}
+
+takeOrder('mushroom', 'thin crust');
+takeOrder('spinach', 'whole wheat');
+takeOrder('pepperoni', 'brooklyn style');
+
+console.log(getSubTotal(orderCount));
