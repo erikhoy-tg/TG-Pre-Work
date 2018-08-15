f46319acf8ea131168bbdcd36f1f9bda737c9bcf
Erik Hoy <erikshoy@gmail.com>
2018-08-15 12:50:58 -0600

    JS Lesson 9 - 12/15

--- css/main.js
+++ css/main.js
@@ -1,36 +1,12 @@
 let person = {
-    name: 'Erik',
-    age: 44,
-    weekendAlarm: 'No alarms needed',
-    weekAlarm: 'Alarm set to 7am',
-    hobbies: ['making art', 'skateboarding'],
-    sayHello: function() {
-      return `Hello, my name is ${this.name}`;
-    },
-    sayGoodbye() { 
-        return 'Goodbye!';
-    }
+    _name: 'Lu Xun',
+    _age: 137,
+      set age(age) {
+      if(typeof age === 'number') {
+        this._age = age;
+      } else {
+        return 'Invalid input';
+      }
+      }
   };
   
-  let friend = {
-    name: 'Judy'
-  }
-  
-  friend.sayHello = person.sayHello;
-  
-  let day = 'Wednesday';
-  let alarm;
-  
-  if(day === 'Saturday' || day === 'Sunday') {
-    alarm = 'weekendAlarm';
-  } else {
-    alarm = 'weekAlarm';
-  }
-  
-  person.hobbies = ['making stuff'];
-  console.log(person['name']);
-  console.log(person['age']);
-  console.log(person[alarm]);
-  console.log(person['hobbies']);
-  console.log(person.sayHello());
-  console.log(friend.sayHello());
