7e2ce765582b62f2299dbdffb557a8fe8083bdda
Erik Hoy <erikshoy@gmail.com>
2018-08-15 08:52:59 -0600

    JS Lesson 3 - 1/11

--- css/app.js
+++ css/app.js
@@ -1,3 +1,12 @@
-let myName = 'Erik';
-let myCity = 'Philadelphia';
-console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+let userName = 'Erik';
+let knowsJavaScript = true;
+
+if (knowsJavaScript && userName) {
+  console.log('Great, ' + userName + '! Get ready to practice your JavaScript!');
+} else if (knowsJavaScript) {
+  console.log('Great! Get ready to practice your JavaScript!');
+} else if (userName) {
+  console.log('Great, ' + userName + '! Get ready to learn something new!');
+} else {
+  console.log('Great! Get ready to learn something new!');
+}
