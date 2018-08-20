108821cda82d18e7eb696c55870dca59530aec42
Erik Hoy <erikshoy@gmail.com>
2018-08-20 15:28:10 -0600

    JS Lesson 14 - 3/17

--- css/main.js
+++ css/main.js
@@ -1,13 +1,8 @@
-const xhr = new XMLHttpRequest();
-const url = "https://api-to-call.com/endpoint";
-const data = JSON.stringify({id: '200'});
-
-xhr.responseType = 'json';
-xhr.onreadystatechange = () => {
-  if (xhr.readyState === XMLHttpRequest.DONE) {
-    return xhr.response;
+fetch('https://api-to-call.com/endpoint').then(response => {
+  if (response.ok) {
+    return response.json();
   }
-}
-
-xhr.open("POST", url);
-xhr.send(data);
+  throw new Error('Request failed!');
+}, networkError => (console.log(networkError.message))).then(jsonResponse => {
+  return jsonResponse;
+});
