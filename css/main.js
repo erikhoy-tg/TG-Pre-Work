1710636b4a21768a2045503487952fcb4c3efd17
Erik Hoy <erikshoy@gmail.com>
2018-08-20 16:36:03 -0600

    JS Lesson 14 - 9/17

--- css/main.js
+++ css/main.js
@@ -12,7 +12,16 @@ const shortenUrl = () => {
   const urlToShorten = inputField.value;
   const data = JSON.stringify({destination: urlToShorten});
   
-  fetch(url, {method: 'POST', headers: {'Content-type': 'application/json', 'apikey': apiKey}, body: data});
+  fetch(url, {method: 'POST', headers: {'Content-type': 'application/json', 'apikey': apiKey}, body: data}).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  },
+	(networkError => {
+    console.log(networkError.message);
+  })                           
+    );
 }
 
 // Clear page and call AJAX functions
